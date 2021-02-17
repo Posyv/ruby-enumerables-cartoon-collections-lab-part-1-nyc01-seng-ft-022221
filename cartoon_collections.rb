5996a622edc82126c0774fe36a5ee08584f35b72
Posy Natalie Vogt <nataliepvogt@gmail.com>
2021-02-17 20:13:17 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,23 @@
 def greet_characters(array)
+  dcount = 0
+  array.each do |dwarf|
+    puts "Hello #{dwarf}!"
+    dcount += 1
+  end
   # Use `each` to enumerate over the provided array
   #
   # Print a custom greeting for each element
 end
 
 def list_dwarves(array)
+  dcount = 0
+  n = 0
+  array.each do |dwarf|
+    puts "#{n+1}" + "#{dwarf}!"
+    dcount += 1
   # Use `each_with_index` to enumerate over the provided array
   #
   # Print a numbered list of each element
+  n += 1
+end
 end
